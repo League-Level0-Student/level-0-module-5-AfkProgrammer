d3dd7fd8b335d7ad9a274a4365913ad8fb289ce6
AfkProgrammer <vidriohans@gmail.com>
2020-10-10 18:24:26 +0000

    On my last assignment for Level 0 Module 5, getting ready for my test.

--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -4,24 +4,35 @@ import javax.swing.JOptionPane;
 public class HappyPet {
 	
 	// 2. Add the following variable to the next line: static int happinessLevel = 0;
+	static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
 	
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
+		String XD = JOptionPane.showInputDialog(null,"What pet do you want?");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
 		
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
+		
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+			int task = JOptionPane.showOptionDialog(null, "What makes your pet happy?", "Happiness of your pet", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "Pet your pet", "Getting punched", "Treats" }, null);
 
 			// 5. Use user input to call the appropriate method created in step 4.
+			if(task==0) {
+				bark();
+				
+			}
 
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
 
+	}
+	
+	private static void bark() {
+		
+		
 	}
 
 	// 4. Create methods to handle each of your user selections.
